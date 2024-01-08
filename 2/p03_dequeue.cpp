7a2ad05984a34147122af06593ca1ee91872b269
Dhiraj Thapa <dhirajthapaparnami@gmail.com>
2024-01-08 23:26:33 +0545

    Completed deque

--- 2/p03_dequeue.cpp
+++ 2/p03_dequeue.cpp
@@ -3,11 +3,21 @@ using namespace std;
 
 const int MAX_SIZE = 50;
 
-template <typename T>
+template <typename T> //circular banaune eslai
+
 class Deque {
     private:
         int front, end;
         int arr[MAX_SIZE];
+        int incCircular(int a) {
+            a = (a + 1) % MAX_SIZE;
+            return a;
+        }
+        int decCircular(int a) {
+            a = a - 1;
+            if(a < 0) a += MAX_SIZE;
+            return a;
+        }
     public:
         Deque() {
             front = end = -1;
@@ -19,43 +29,81 @@ class Deque {
                 exit(0);
             } else {
                 if(front == -1 && end == -1) {
-                    front = end = MAX_SIZE / 2;
+                    front = end = 0;
                 } else 
-                    front -= 1;
+                    front = decCircular(front);
             }
             arr[front] = val;
         }
 
-        void addEnd() {
+        void addEnd(T val) {
             if(isFull()) {
                 cerr<<"Queue Overflow!"<<endl;
                 exit(-1);
             } else {
                 if(front == -1 && end == -1) {
-                    front = end = MAX_SIZE /2;
+                    front = end = 0;
                 } else 
-                    end += 1;
+                    end = incCircular(end);
             }
             arr[end] = val;
         }
 
-        int removeBegin() {
+        T removeBegin() {
+            T value;
             if(isEmpty()) {
                 cerr<<"Queue Undeflow!"<<endl;
+            } else {
+                value = arr[front];
+                front = incCircular(front);
             }
+            return value;
         }
 
-        int removeEnd() {
+        T removeEnd() {
+            T value;
             if(isEmpty()) {
                 cerr<<"Queue Undeflow!"<<endl;
+            } else {
+                value = arr[end];
+                end = decCircular(end);
             }
+            return value;
         }
 
         bool isFull() {
-
+            if(front == incCircular(end)) return true;
+            return false;
         }
 
         bool isEmpty() {
+            if(front == -1 && end == -1) return true;
+            return false;
+        }
 
+        void printQueue() {
+            int i;
+            for(i = front; i != end; i = incCircular(i)) {
+                cout<<arr[i]<<' ';
+            }
+            cout<<arr[i];
+        }
+        void status() {
+            cout<<"Front = "<<front<<"back = "<<end<<endl;
         }
 };
+
+int main() {
+    Deque<int> q;
+    q.addBegin(5);
+    q.addBegin(6);
+    q.addEnd(7);
+    
+    q.addBegin(5);
+    q.addBegin(7);
+    q.addEnd(8);
+    q.removeEnd();
+    q.removeBegin();
+    q.printQueue();
+    return 0;
+}
